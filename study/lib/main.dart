fdbc205114f7c643395683bbb924f7facab6be40
yeonwookim1 <kop4845@naver.com>
2023-05-17 20:28:20 +0900

    flutter front

--- study/lib/main.dart
+++ study/lib/main.dart
@@ -11,34 +11,37 @@ class App extends StatelessWidget {
     return MaterialApp(
       //new 생략 //named parameter, 클래스 필드에 ? 붙은 건는 필수가 아님
       home: Scaffold(
-          backgroundColor: Color(0xFF181818),
-          body:
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 40),
-              child: Column(children: [
-                SizedBox(height: 80),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.end,
-                      children: [
-                        Text('Hey, Kyw',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 28,
-                                fontWeight: FontWeight.w800)),
-                        Text(
-                          'Welcome',
-                          style: TextStyle(color: Colors.white.withOpacity(0.8), fontSize: 18),
-                        )
-                      ],
-                    )
-                  ],
-                )
-              ]),
-            ),
-
+        backgroundColor: Color(0xFF181818),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Column(
+            children: [
+              SizedBox(height: 80),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.end,
+                    children: [
+                      Text(
+                        'Hey, Kyw',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 28,
+                            fontWeight: FontWeight.w800),
+                      ),
+                      Text(
+                        'Welcome',
+                        style: TextStyle(
+                            color: Colors.white.withOpacity(0.8), fontSize: 18),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
